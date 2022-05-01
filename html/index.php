9d9c4c5bc649f6376670944cd1c7e5181876c45f
David <david99111@mail.ru>
2022-05-01 11:42:57 +0300

    refactor

--- html/index.php
+++ html/index.php
@@ -30,28 +30,21 @@
                 <a class="nav-link" id="tab-register" data-mdb-toggle="pill" href="#pills-register" role="tab" aria-controls="pills-register" aria-selected="false">Register</a>
             </li>
         </ul>
-        <!-- Pills navs -->
 
-        <!-- Pills content -->
         <div class="tab-content">
             <div class="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
                 <form>
 
-                    <!-- Email input -->
                     <div class="form-outline mb-4">
                         <input type="text" id="loginName" class="form-control" />
                         <label class="form-label" for="loginName">Username</label>
                     </div>
 
-                    <!-- Password input -->
                     <div class="form-outline mb-4">
                         <input type="password" id="loginPassword" class="form-control" />
                         <label class="form-label" for="loginPassword">Password</label>
                     </div>
 
-
-
-                    <!-- Submit button -->
                     <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
 
 
@@ -60,39 +53,27 @@
             <div class="tab-pane fade" id="pills-register" role="tabpanel" aria-labelledby="tab-register">
                 <form>
 
-                    <!-- Name input -->
                     <div class="form-outline mb-4">
-                        <input type="text" id="registerName" class="form-control" />
-                        <label class="form-label" for="registerName">Name</label>
+                        <input type="text" id="registerUserName" class="form-control" />
+                        <label class="form-label" for="registerName">UserName</label>
                     </div>
 
-                    <!-- Username input -->
                     <div class="form-outline mb-4">
-                        <input type="text" id="registerUsername" class="form-control" />
-                        <label class="form-label" for="registerUsername">Username</label>
+                        <input type="email" id="registerEmail" class="form-control" />
+                        <label class="form-label" for="registerUsername">Email</label>
                     </div>
 
-                    <!-- Password input -->
                     <div class="form-outline mb-4">
                         <input type="password" id="registerPassword" class="form-control" />
                         <label class="form-label" for="registerPassword">Password</label>
                     </div>
 
-                    <!-- Repeat Password input -->
-                    <div class="form-outline mb-4">
-                        <input type="password" id="registerRepeatPassword" class="form-control" />
-                        <label class="form-label" for="registerRepeatPassword">Repeat password</label>
-                    </div>
-
-
-
-                    <!-- Submit button -->
                     <button type="submit" class="btn btn-primary btn-block mb-3">Sign Up</button>
                 </form>
             </div>
         </div>
     </section>
-    <!-- Pills content -->
+
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.0.0/mdb.min.js"></script>
 
 </body>
